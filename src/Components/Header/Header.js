cb0d853943d49efeed549a25b552dfb657925723
abdullah-miah <mdabdullaha708@gmail.com>
2022-04-04 22:14:48 +0600

    Header section complete

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../Header/Header.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='logo'>
+              <h4>food <span>.</span> 
+              <small> Corner</small>
+              </h4>
+            </div>
+            <div className='link'>
+            <Link to ='/' > HOME</Link>
+                <Link to ='/review'> REVIEWS</Link>
+                <Link to ='/dashboard' > DASHBOARD</Link>
+                <Link to ='/blogs' > BLOGS</Link>
+                <Link to ="/about">About</Link>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
