eac72b3ecc1c1a0de579d618ef8585c8f0ecb0c3
abdullah-miah <mdabdullaha708@gmail.com>
2022-04-05 12:05:51 +0600

    rechart setup

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,10 +1,33 @@
-import React from 'react';
 
+import React, { useEffect, useState } from 'react';
+import { CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import '../Dashboard/Dashboard.css';
 const Dashboard = () => {
+    const [chart, setChart]=useState([])
+    useEffect(()=>{
+        fetch('data.json')
+        .then(res =>res.json())
+        .then(data=>setChart(data))
+    },[])
     return (
-        <div>
-            <h1>hello dashboard</h1>
-        </div> 
+        <div className='Chart'>
+            <div className='line-chart'>
+                <h4> Sell per Month</h4>
+            <LineChart  width={600} height={400} data={chart}>
+            <Line type="monotone" dataKey="sell" stroke="#8884d8"></Line>
+            <XAxis dataKey="month"></XAxis>
+            <YAxis></YAxis>
+            <CartesianGrid></CartesianGrid>
+            <Tooltip></Tooltip>
+            </LineChart>
+            </div>
+            <div>
+            <LineChart  width={400} height={400} data={chart}>
+            <Line type="monotone" dataKey="sell" stroke="#8884d8"></Line>
+            </LineChart>
+            </div>
+           
+        </div>
     );
 };
 
